29e4cc0ca5e1ad77ea68c09c8dcca813679c78b5
ksc <ksc036@gmail.com>
2025-04-18 01:02:08 +0900

    health check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <h1>Test</h1>
         <ol>
           <li>
             Get started by editing <code>src/app/page.tsx</code>.
