17251069a699c45a2747cc6d2a31c7f9453b454a
ksc <ksc036@gmail.com>
2025-04-19 03:51:03 +0900

    build test

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -30,7 +30,7 @@ export default async function ProfilePage({ params }: Props) {
     <div className={styles.container}>
       <div className={styles.content}>
         <div className={styles.menu}>
-          <div className={styles.profile}>님의 프로필입니다.</div>
+          <div className={styles.profile}>${id}님의 프로필입니다.</div>
           <div className={styles.tag}>태그들</div>
         </div>
         <CardList posts={posts}></CardList>
