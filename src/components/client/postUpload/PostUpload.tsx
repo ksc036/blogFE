4a44726d5a97d458e6c9e426d86844d5caa08c8a
ksc <ksc036@gmail.com>
2025-04-23 04:18:45 +0900

    posts

--- src/components/client/postUpload/PostUpload.tsx
+++ src/components/client/postUpload/PostUpload.tsx
@@ -43,7 +43,7 @@ export default function PostUpload({
       const id = res.data.postId; // 서버에서 반환해주는 고유 URL
       // setShowPublishScreen(false);
       console.log(id);
-      router.push(`post/${id}`); // Next.js의 클라이언트 라우팅
+      router.push(`posts/${id}`); // Next.js의 클라이언트 라우팅
       // alert("포스트가 성공적으로 업로드되었습니다.");
     } catch (err) {
       console.error("포스트 업로드 실패", err);
