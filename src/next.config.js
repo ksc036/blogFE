f605e1ee47ada21a5864dcf5aa63e6250d1b017c
ksc <ksc036@gmail.com>
2025-04-28 06:07:25 +0900

    path change

--- src/next.config.js
+++ src/next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: "/:path*", // 요청 경로는 뭐든
-        has: [{ type: "host", value: "(?<user>[^.]+)\\.example\\.com" }],
+        has: [{ type: "host", value: "(?<user>[^.]+)\\.ksc036\\.store" }],
         destination: "/profile/:user", // 항상 /profile/서브도메인 으로
       },
     ];
