00765d58677aa56e04739bc223cb7384c3db6f40
ksc <ksc036@gmail.com>
2025-05-01 19:31:00 +0900

    profilepage

--- src/app/(pages)/[subdomain]/page.tsx
+++ src/app/(pages)/[subdomain]/page.tsx
@@ -1,47 +1,54 @@
 import axiosInstance from "@/shared/lib/axiosInstance";
 import styles from "./BlogPage.module.css";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default async function BlogPage({ params }) {
   const { subdomain } = await params;
   const res = await axiosInstance.get(`users/blogProfile/${subdomain}`);
   const data = res.data;
+  console.log(data);
+  if (!data) {
+    return <div>사용자없음</div>;
+  }
   return (
     <main className={styles.container}>
       <section className={styles.profileSection}>
-        <Link href={`/posts/${data.post.id}`}>
-          <Image
-            src={data.user.thumbnailUrl}
-            alt="User profile"
-            width={80}
-            height={80}
-            className={styles.avatar}
-          />
-          <div className={styles.profileInfo}>
-            <h2 className={styles.name}>{data.user.blogName}</h2>
-            <p className={styles.bio}>{data.user.bio}</p>
-          </div>
-        </Link>
+        <Image
+          src={data.user.thumbnailUrl}
+          alt="User profile"
+          width={80}
+          height={80}
+          className={styles.avatar}
+        />
+        <div className={styles.profileInfo}>
+          <h2 className={styles.name}>{data.user.blogName}</h2>
+          <p className={styles.bio}>{data.user.bio}</p>
+        </div>
       </section>
 
       {/* 포스트 목록 */}
       <section className={styles.postList}>
         {data.posts.map((post) => (
-          <article key={post.id} className={styles.postCard}>
-            <Image
-              src={post.thumbnailUrl}
-              width={80}
-              height={80}
-              className={styles.thumbnail}
-              alt="post-thumbnail"
-            />
-            <div className={styles.postContent}>
-              <h3 className={styles.postTitle}>{post.title}</h3>
-              <p className={styles.postSummary}>{post.desc}</p>
-            </div>
-          </article>
+          <Link
+            href={`https://${post.user.subdomain}.ksc036.store/posts/${[
+              post.id,
+            ]}`}
+          >
+            <article key={post.id} className={styles.postCard}>
+              <Image
+                src={post.thumbnailUrl}
+                width={80}
+                height={80}
+                className={styles.thumbnail}
+                alt="post-thumbnail"
+              />
+              <div className={styles.postContent}>
+                <h3 className={styles.postTitle}>{post.title}</h3>
+                <p className={styles.postSummary}>{post.desc}</p>
+              </div>
+            </article>
+          </Link>
         ))}
       </section>
     </main>
