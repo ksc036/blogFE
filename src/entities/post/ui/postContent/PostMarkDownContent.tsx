a6aded5557c7b263fc5b863a6423e7d439fd00d8
ksc <ksc036@gmail.com>
2025-05-03 07:42:43 +0900

    img size

--- src/entities/post/ui/postContent/PostMarkDownContent.tsx
+++ src/entities/post/ui/postContent/PostMarkDownContent.tsx
@@ -2,5 +2,20 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 export default function PostContent({ content }: { content: string }) {
-  return <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>;
+  return (
+    <ReactMarkdown
+      components={{
+        img: ({ node, ...props }) => (
+          <img
+            {...props}
+            style={{ height: "auto", maxWidth: "100%" }} // 예시
+            alt={props.alt}
+          />
+        ),
+      }}
+      remarkPlugins={[remarkGfm]}
+    >
+      {content}
+    </ReactMarkdown>
+  );
 }
