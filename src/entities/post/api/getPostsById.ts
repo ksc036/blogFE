149bd9e9247c6cdcb93e37bf5915bf9202378888
ksc <ksc036@gmail.com>
2025-05-03 21:01:23 +0900

    쿠키 세팅팅

--- src/entities/post/api/getPostsById.ts
+++ src/entities/post/api/getPostsById.ts
@@ -1,5 +1,14 @@
 import axiosInstance from "@/shared/lib/axiosInstance";
+import { cookies } from "next/headers";
 export async function getPostsById(id: number) {
-  const res = await axiosInstance.get(`/posts/${id}`);
+  const cookieStore = await cookies();
+  // const token = cookieStore.get("token")?.value;
+  const cookieHeader = cookies().toString(); // "token=...; other=value"
+  console.log("token,", cookieHeader);
+  const res = await axiosInstance.get(`/posts/${id}`, {
+    headers: {
+      Cookie: cookieHeader, // ✅ key=value 형식으로 명시
+    },
+  });
   return res.data;
 }
