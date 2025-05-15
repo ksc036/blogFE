d880899c623de611164f2b17875ce29fefc3da10
ksc <ksc036@gmail.com>
2025-05-16 06:15:17 +0900

    cursor pointer 추가가

--- src/features/post/ui/PostDeleteButton.tsx
+++ src/features/post/ui/PostDeleteButton.tsx
@@ -3,5 +3,9 @@ import { useAppSelector } from "@/shared/store/hooks";
 import { usePostDelete } from "../model/usePostDelete";
 export default function PostDeleteButton({ postId }: { postId: number }) {
   const { handleDelete } = usePostDelete(postId);
-  return <div onClick={handleDelete}>삭제</div>;
+  return (
+    <div style={{ cursor: "pointer" }} onClick={handleDelete}>
+      삭제
+    </div>
+  );
 }
--- src/features/post/ui/PostEditButton.tsx
+++ src/features/post/ui/PostEditButton.tsx
@@ -4,6 +4,13 @@ import { useRouter } from "next/navigation";
 
 export default function postEdit({ postId }: { postId: number }) {
   const router = useRouter();
-  return <div onClick={() => router.push(`/write/${postId}`)}>수정</div>;
+  return (
+    <div
+      style={{ cursor: "pointer" }}
+      onClick={() => router.push(`/write/${postId}`)}
+    >
+      수정
+    </div>
+  );
   // return <div onClick={() => router.push(`/write`)}>수정</div>;
 }
