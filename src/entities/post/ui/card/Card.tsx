cd697008dcd9949406773153153a3d9eddcd9b84
ksc <ksc036@gmail.com>
2025-05-16 00:41:16 +0900

    card a태그 중첩제거거

--- src/entities/post/ui/card/Card.tsx
+++ src/entities/post/ui/card/Card.tsx
@@ -7,40 +7,42 @@ import { formatToKoreanDate } from "@/shared/lib/date/formatData";
 export default function Card({ post }: { post: Post }) {
   console.log("Card", post);
   return (
-    <Link
-      href={`https://${post.user.subdomain}.ksc036.store/posts/${post.postUrl}`}
-    >
-      {/* <Link href={`/posts/${post.id}`}> */}
-      <div className={styles.card}>
-        <div className={styles.thumbnail}>
-          <img
-            className={styles.img}
-            src={
-              post.thumbnailUrl
-                ? post.thumbnailUrl
-                : "https://minio.ksc036.store/log404default/default-thumbnail.png"
-            }
-            alt=""
-          />
-        </div>
+    <div className={styles.card}>
+      <Link
+        href={`https://${post.user.subdomain}.ksc036.store/posts/${post.postUrl}`}
+      >
+        {/* <Link href={`/posts/${post.id}`}> */}
+        <div>
+          <div className={styles.thumbnail}>
+            <img
+              className={styles.img}
+              src={
+                post.thumbnailUrl
+                  ? post.thumbnailUrl
+                  : "https://minio.ksc036.store/log404default/default-thumbnail.png"
+              }
+              alt=""
+            />
+          </div>
 
-        <div className={styles.main}>
-          <div className={styles.title}>{post.title}</div>
-          <div className={styles.content}>{post.desc}</div>
-        </div>
-        <div className={styles.info}>
-          <div className={styles.date}>
-            {formatToKoreanDate(post.createdAt)}
+          <div className={styles.main}>
+            <div className={styles.title}>{post.title}</div>
+            <div className={styles.content}>{post.desc}</div>
           </div>
-          <div className={styles.cardFooter}>
-            <PostUserProfile user={post.user}></PostUserProfile>
-            <PostLike
-              likeCnt={post._count.likes}
-              commentCnt={post.commentCount}
-            ></PostLike>
+          <div className={styles.info}>
+            <div className={styles.date}>
+              {formatToKoreanDate(post.createdAt)}
+            </div>
           </div>
         </div>
+      </Link>
+      <div className={styles.cardFooter}>
+        <PostUserProfile user={post.user}></PostUserProfile>
+        <PostLike
+          likeCnt={post._count.likes}
+          commentCnt={post.commentCount}
+        ></PostLike>
       </div>
-    </Link>
+    </div>
   );
 }
