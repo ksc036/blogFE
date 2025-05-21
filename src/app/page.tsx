680d966595344896262d8229434fb8f2a8a6c8e2
ksc <ksc036@gmail.com>
2025-05-22 04:29:57 +0900

    캐싱 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import styles from "./page.module.css";
 import CardList from "@/widgets/postList/ui/PostList/CardList";
 import { getPosts } from "@/entities/post/api/getPosts";
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  console.log("Home page init");
   const posts = await getPosts();
-  console.log("home posts list ", posts);
   return (
     <div className={styles.page}>
       <CardList posts={posts}></CardList>
