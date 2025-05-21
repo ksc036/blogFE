04a5ceb808dc942d3a7682f506e6387e5b95279f
ksc <ksc036@gmail.com>
2025-05-21 21:25:27 +0900

    url 수정정

--- src/app/(pages)/[subdomain]/page.tsx
+++ src/app/(pages)/[subdomain]/page.tsx
@@ -11,7 +11,7 @@ import { Post } from "@/entities/post/model/types";
 export default async function BlogPage({ params }: urlParams) {
   console.log("BlogPage rendering..");
   const { subdomain } = await params;
-  const res = await axiosInstance.get(`users/blogProfile/${subdomain}`);
+  const res = await axiosInstance.get(`/users/blogProfile/${subdomain}`);
   const data = res.data;
   console.log("data==", data);
 
